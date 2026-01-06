66d8fa865350c4dd44b2cc1304a84667b1b54207
Joshua Smith <joshua.barton.smith12@gmail.com>
2026-01-06 15:44:37 -0500

    move weird dynamic statedefs to function, fix workflow calling devils

--- MakeDevils.py
+++ MakeDevils.py
@@ -8,18 +8,21 @@ def _devils():
     if Name != "M-Creirwy" or (IsHelper() and root.Name != "M-Creirwy"):
         ChangeState(value = "TargetLandingState", ignorehitpause = True)
 
+def dynamicState(idx, state):
+    statedef(stateno = idx, scope = SCOPE_TARGET)(state)
+
 if __name__ == "__main__":
 
     ## states from 0 to 1k, all
     for idx in range(1000):
         next_state = copy.deepcopy(_devils)
         next_state.__name__ = f"DevilsEye{idx}"
-        statedef(stateno = idx, scope = SCOPE_TARGET)(next_state)
+        dynamicState(idx, next_state)
 
     ## states from 1k to 1m, every 1k
     for idx in range(1000, 1000000, 1000):
         next_state = copy.deepcopy(_devils)
         next_state.__name__ = f"DevilsEye{idx}"
-        statedef(stateno = idx, scope = SCOPE_TARGET)(next_state)
+        dynamicState(idx, next_state)
 
     build("M-Creirwy.def", "States/Creirwy-Devil.mtl", run_mtl=False, locations=False, compress=True, preserve_ir=True)
